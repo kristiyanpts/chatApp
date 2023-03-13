19e66d0b66524d7af1f81682fa59fcdbbb152fd1
Kristiyan Petsanov <kristiqn3456@gmail.com>
2023-03-13 10:15:47 +0200

    a

--- src/register.js
+++ src/register.js
@@ -4,4 +4,8 @@ let registerPage = document.querySelector(".register-page");
 export function showRegisterPage() {
   hideSections();
   registerPage.style.display = "flex";
+  let form = document.querySelector("#register-form");
+  form.addEventListener("submit", registerUser);
 }
+
+async function registerUser() {}
