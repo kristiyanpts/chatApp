6d3041b70c69f306238cbe2510aa9753d2d71464
Kristiyan Petsanov <kristiqn3456@gmail.com>
2023-04-01 20:58:02 +0300

    asd

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/app.js",
+  mode: "development",
+  output: {
+    path: path.resolve("", "dist"),
+    filename: "bundle.js",
+  },
+};
